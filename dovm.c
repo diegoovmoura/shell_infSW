67afd0faca619264dcc2f37685360fa6e363487c
Diego Moura <diegoovmoura@gmail.com>
2023-03-12 07:02:52 -0300

    background commands added

--- dovm.c
+++ dovm.c
@@ -7,8 +7,9 @@
 #define MAX_LINE_LENGTH 41
 
 
-void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid);
-void run_parallel(char args[MAX_LINE_LENGTH], pid_t pid);
+void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid, int *back_count, pid_t *back_pids);
+void run_parallel(char args[MAX_LINE_LENGTH], pid_t pid, int *back_count, pid_t *back_pids);
+void pipe_func(int has_pipe, char *command_args_pipe[], char *command_args[], int fd[]);
 
 
 int main(int argc, char *argv[])
@@ -16,6 +17,8 @@ int main(int argc, char *argv[])
     char args[MAX_LINE_LENGTH];	// command line has max of 40 arguments
     char save[MAX_LINE_LENGTH]; // copty of the command line for the history
     int has_save = 0;
+    int back_count = 0;
+    pid_t back_pids[10] = {0};
     int should_run = 1, verify_style = 1;
     pid_t pid;
     FILE *file;
@@ -85,14 +88,14 @@ int main(int argc, char *argv[])
                         printf("No commands\n");
                         continue;
                     }                 
-                    run_sequential(save, pid); // passing the previous command line
+                    run_sequential(save, pid, &back_count, (pid_t *)&back_pids); // passing the previous command line
                     continue;
                 }
                 if (strcmp(command_args[i], "style parallel") == 0) { //change style
                     verify_style = 0;
                     continue;
                 }
-                run_sequential(command_args[i], pid);
+                run_sequential(command_args[i], pid, &back_count, (pid_t *)&back_pids);
                 if (strlen(command_args[i]) > 1)
                 {
                     strcpy(save, command_args[i]);
@@ -108,14 +111,14 @@ int main(int argc, char *argv[])
                         printf("No commands\n");
                         continue;
                     }
-                    run_sequential(save, pid); // passing the previous command line
+                    run_sequential(save, pid, &back_count, (pid_t *)&back_pids); // passing the previous command line
                     continue;
                 }
                 if (strcmp(command_args[i], "style sequential") == 0) { //change style
                     verify_style = 1;
                     continue;
                 } 
-                run_parallel(command_args[i], pid);
+                run_parallel(command_args[i], pid, &back_count, (pid_t *)&back_pids);
                 if (strlen(command_args[i]) > 1)
                 {
                     strcpy(save, command_args[i]);
@@ -152,9 +155,9 @@ int main(int argc, char *argv[])
                     {
                         printf("No commands\n");
                     }                
-                    run_sequential(save, pid);
+                    run_sequential(save, pid, &back_count, (pid_t *)&back_pids);
                     continue;
-                }          
+                }       
                 if (strcmp(args, "exit") == 0) // exit 
                 {  
                     break;
@@ -171,7 +174,7 @@ int main(int argc, char *argv[])
                 }        
 
                 // runing the command line in terminal
-                run_sequential(args, pid);
+                run_sequential(args, pid, &back_count, (pid_t *)&back_pids);
 
             }
             // parallel mode
@@ -196,7 +199,7 @@ int main(int argc, char *argv[])
                     {
                         printf("No commands\n");
                     }                
-                    run_parallel(save, pid);
+                    run_parallel(save, pid, &back_count, (pid_t *)&back_pids);
                     continue;
                 }        
                 if (strcmp(args, "exit") == 0) { //exit
@@ -213,7 +216,7 @@ int main(int argc, char *argv[])
                 }     
 
                 // runing the command line in terminal
-                run_parallel(args, pid);      
+                run_parallel(args, pid, &back_count, (pid_t *)&back_pids);      
             }  
         }
     }
@@ -221,20 +224,24 @@ int main(int argc, char *argv[])
 }
 
 
-void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid)
+void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid, int *back_count, pid_t *back_pids)
 {
-    int lock = 0;
+    int a = 0, lock = 0;
+    int back = 0;
     // slicing the line of commands
-    char *slice = strtok(args, ";"); 
+    char *slice = strtok(args, ";");
     while (slice != NULL)
     {
+        int has_pipe = 0;
+        int fd[2];
         char *command_args[20] = {0};
+        char *command_args_pipe[20] = {0};
         char aux[20][20] = {0};
         
         // slicing again to take the command arguments
         int count = 0, j = 0;
         for (int i = 0; i < strlen(slice); i++)
-        {              
+        {      
             // taking off unecessaries spaces
             if (i == 0 && slice[i] == ' ')
             {
@@ -244,9 +251,35 @@ void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid)
             {
                 continue;
             }
-            
             else if (i > 0 && slice[i - 1] == ' ' && slice[i] == ' ')
             {
+                continue;
+            }
+            else if (slice[i] == ' ' && slice[i+1] == '&')
+            {
+                continue;
+            }
+            else if (slice[i] == '&')
+            {                
+                back = 1;
+                continue;
+            }
+            
+            if (slice[i] == '|')
+            {
+                has_pipe = 1;
+                slice[i] = 0;
+                slice[i+1] = 0;
+                for (int i = 0; i < count +1; i++)
+                {
+                    command_args_pipe[i] = aux[i];   
+                    for (int j = 0; j < count; j++)
+                    {
+                        aux[i][j] = 0;
+                    }
+                    count = 0;
+                    j = 0;
+                }
                 continue;
             }
 
@@ -268,19 +301,51 @@ void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid)
             command_args[i] = aux[i];   
         }
 
-        pid = fork();
-
-        if (pid > 0) // shell waits for the end of the commands processes
+        if (strcmp(command_args[0], "fg") == 0)
         {
-            wait(NULL);
+            printf("process .out\n");
+            int aux = atoi(command_args[1]);
+            int w_status;
+            waitpid(back_pids[aux], &w_status, 0);
+            break;
         }
-        else // run the command in terminal and ends the child process
+        
+        
+        if (has_pipe == 1)
         {
-            if (execvp(command_args[0], command_args) == -1)
+            pipe_func(has_pipe, command_args, command_args_pipe, fd);
+            continue;
+        } 
+
+        int pid = fork();
+        if (back == 1)
+        {
+            (*back_count)++;
+            if (pid > 0)
             {
-                printf("%s: command not found\n", command_args[0]);
+                printf("[%d] %d\n", *back_count, pid);
+                back_pids[(*back_count) - 1] = pid;
+                if (execvp(command_args[0], command_args) == -1)
+                {
+                    printf("%s: command not found\n", command_args[0]);
+                }
             }
-            exit(pid);
+        }
+        else
+        {
+            if (pid > 0) // shell waits for the end of the commands processes
+            {
+                wait(NULL);
+            }
+            else 
+            {
+                if (execvp(command_args[0], command_args) == -1)
+                {
+                    printf("%s: command not found\n", command_args[0]);
+                }
+                exit(pid);
+            }
+
         }
 
         slice = strtok(NULL, ";");
@@ -288,9 +353,11 @@ void run_sequential(char args[MAX_LINE_LENGTH], pid_t pid)
 }
 
 
-void run_parallel(char args[40], pid_t pid)
+void run_parallel(char args[40], pid_t pid, int *back_count, pid_t *back_pids)
 {
     int a = 0, lock = 0;
+    int pipe = 0;
+    int back = 0;
     // slicing the line of commands
     char *slice = strtok(args, ";");
     while (slice != NULL)
@@ -311,9 +378,22 @@ void run_parallel(char args[40], pid_t pid)
             {
                 continue;
             }
-            
             else if (i > 0 && slice[i - 1] == ' ' && slice[i] == ' ')
             {
+                continue;
+            }
+            else if (slice[i] == '|')
+            {
+                pipe = 1;
+                slice[i] = 0;
+            }
+            else if (slice[i] == ' ' && slice[i+1] == '&')
+            {
+                continue;
+            }
+            else if (slice [i] == '&')
+            {
+                back = 1;
                 continue;
             }
 
@@ -335,9 +415,31 @@ void run_parallel(char args[40], pid_t pid)
             command_args[i] = aux[i];   
         }
 
+        if (strcmp(command_args[0], "fg") == 0)
+        {
+            printf("process .out\n");
+            int aux = atoi(command_args[1]);
+            int w_status;
+            waitpid(back_pids[aux], &w_status, 0);
+            break;
+        }
+
         pid = fork();
 
-        if (pid == 0) // run the command in terminal and ends the child process
+        if (back == 1)
+        {
+            (*back_count)++;
+            if (pid > 0)
+            {
+                printf("[%d] %d\n", *back_count, pid);
+                back_pids[(*back_count) - 1] = pid;
+                if (execvp(command_args[0], command_args) == -1)
+                {
+                    printf("%s: command not found\n", command_args[0]);
+                }
+            }
+        }
+        else if (pid == 0) // run the command in terminal and ends the child process
         {
             if (execvp(command_args[0], command_args) == -1)
             {
@@ -355,4 +457,31 @@ void run_parallel(char args[40], pid_t pid)
             wait(NULL);
         }
     }
+}
+
+void pipe_func(int has_pipe, char *command_args_pipe[], char *command_args[], int fd[])
+{
+    pipe(fd);
+    int pid = fork();
+    if (pid == 0)
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(command_args_pipe[0], command_args_pipe);
+    }
+    
+    int pid2 = fork();
+
+    if (pid2 == 0)
+    {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(command_args[0], command_args);
+    }
+
+    waitpid(pid, NULL, 0);  
+    waitpid(pid2, NULL, 0);  
+    has_pipe = 0;
 }
